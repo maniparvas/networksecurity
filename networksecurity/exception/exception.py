c7ff1a30b9571f7b26ba21fa4a9fe739cf8071c6
maniparvas <mahamaniparvas@gmail.com>
2025-09-13 12:49:12 +0530

    ETL pipeline completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -9,8 +9,9 @@ class NetworkSecurityException(Exception):
         self.file_name=exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format
-        self.file_name,self.lineno, str(self.error_message)
+            return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+                self.file_name, self.lineno, str(self.error_message)
+            )
 
 if __name__=='__main__':
     try:
